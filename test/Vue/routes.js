90a2fc88b9027d4d0cd8df36205f51a17e55e432
NatsukiAmano <85480155+NatsukiAmano@users.noreply.github.com>
2022-10-13 14:34:45 +0900

    Vue側でデータ取得

--- test/Vue/routes.js
+++ test/Vue/routes.js
@@ -21,6 +21,13 @@ const app  = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(DOCUMENT_ROOT))
+// CORSの有効化
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+ });
 
 //-----------------
 // API準備
@@ -31,14 +38,14 @@ app.get("/", async (req, res) => {
   const ans = await models.Ans.findAll()
   // const ans = await models.Ans.findByPk(2)
   res.json(ans)
-  console.log(ans)
+  // console.log(ans)
 })
 
 // QuestionとAnsを紐づけ&～3000/1で表示
 app.get("/1", async(req, res) => {
   const ques = await models.Ans.getAns()
   res.json(ques)
-  console.log(ques)
+  // console.log(ques)
 })
 
 //-----------------
